65f0291b5431ec5845462143eecee42b3eca3870
Jonathan Small <jonathn6@icloud.com>
2018-09-06 20:33:06 -0400

    commit 2

--- server.js
+++ server.js
@@ -5,4 +5,11 @@ const port = process.env.PORT || 3001
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
+/*
+Send request to the app, let it deal with 404s, etc
+ */
+app.use((req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+  })
+
 app.listen(port, () => console.log("Listening on Port", port)) 
